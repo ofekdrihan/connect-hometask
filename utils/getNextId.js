665b06e84c100486a2182e3720cfea2ac3c2f158
ofekdrihan <drihan98@gmail.com>
2025-08-10 00:38:24 +0300

    remove unnecessary files to clean up the project

--- utils/getNextId.js
+++ utils/getNextId.js
@@ -1,10 +1,10 @@
 import Counter from "../models/counter.js";
 
-export async function getNextId(sequenceName) {
+export const getNextId = async (sequenceName) => {
   const counter = await Counter.findOneAndUpdate(
     { _id: sequenceName },
     { $inc: { seq: 1 } },
     { new: true, upsert: true }
   );
   return counter.seq;
-}
+};
--- utils/resetCounters.js
+++ utils/resetCounters.js
@@ -1,7 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import Counter from "../models/counter.js";
 
-export async function resetCounters() {
+dotenv.config();
+
+export const resetCounters = async () => {
   await Counter.updateOne({ _id: "categoryId" }, { $set: { seq: 0 } });
   await Counter.updateOne({ _id: "itemId" }, { $set: { seq: 0 } });
   console.log("Counters reset to 0");
-}
+};
+
+const main = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  await resetCounters();
+  await mongoose.disconnect();
+
+main();
